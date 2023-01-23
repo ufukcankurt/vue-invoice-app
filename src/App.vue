4882230260b4d69ef06aab211abc65f6b775b08b
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-01-23 11:37:59 +0300

    feat(App.vue): create app structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-	<router-view />
+	<div>
+		<div class="app flex flex-column">
+			<NavigationComp />
+			<div class="app-content flex flex-column">
+				<router-view />
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+import NavigationComp from '@/components/NavigationComp.vue';
+export default {
+	components: {
+		NavigationComp,
+	},
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap');
@@ -10,7 +26,20 @@
 	padding: 0;
 	box-sizing: border-box;
 	font-family: 'Poppins', sans-serif;
+}
+
+.app {
 	background-color: #141625;
+	min-height: 100vh;
+	@media (min-width: 900px) {
+		flex-direction: row !important;
+	}
+
+	.app-content {
+		padding: 0 20px;
+		flex: 1;
+		position: relative;
+	}
 }
 
 button,
