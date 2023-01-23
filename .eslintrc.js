ba9964ed516466dece55fd032aae338051f133b5
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-01-23 09:49:51 +0300

    feat(.eslintrc): disable default prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
+    // "plugin:prettier/recommended",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
