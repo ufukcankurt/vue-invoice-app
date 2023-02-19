16b687fa2c6901176c125faeef0022f939ded06a
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-02-19 21:32:55 +0300

    feat(views): create header at home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,47 @@
 <template>
-	<div class="home"></div>
+	<div class="home container">
+		<!-- Header -->
+		<div class="header flex">
+			<div class="left flex flex-column">
+				<h1>Invoices</h1>
+				<span>There are 4 total invoices</span>
+			</div>
+			<div class="right flex">
+				<div
+					@click="toggleFilterMenu"
+					class="filter flex"
+				>
+					<span>Filter by status</span>
+					<img
+						src="@/assets/icon-arrow-down.svg"
+						alt=""
+					/>
+					<!-- Show only filterMenu true -->
+					<ul
+						v-show="filterMenu"
+						class="filter-menu"
+					>
+						<li>Draft</li>
+						<li>Pending</li>
+						<li>Paid</li>
+						<li>Clear Filter</li>
+					</ul>
+				</div>
+				<div
+					@click="newInvoice"
+					class="button flex"
+				>
+					<div class="inner-button flex">
+						<img
+							src="@/assets/icon-plus.svg"
+							alt=""
+						/>
+					</div>
+					<span>New Invoice</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -7,6 +49,99 @@
 
 export default {
 	name: 'HomeView',
+	data() {
+		return {
+			filterMenu: null,
+		};
+	},
 	components: {},
+	methods: {
+		toggleFilterMenu() {
+			this.filterMenu = !this.filterMenu;
+		},
+		newInvoice() {},
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+	color: #fff;
+
+	.header {
+		margin-bottom: 65px;
+
+		.left,
+		.right {
+			flex: 1;
+		}
+
+		.right {
+			justify-content: flex-end;
+			align-items: center;
+
+			.button,
+			.filter {
+				align-items: center;
+
+				span {
+					font-size: 12px;
+				}
+			}
+
+			.filter {
+				cursor: pointer;
+				position: relative;
+				margin-right: 40px;
+
+				img {
+					margin-left: 12px;
+					width: 9px;
+					height: 5px;
+				}
+
+				.filter-menu {
+					width: 120px;
+					position: absolute;
+					top: 25px;
+					list-style: none;
+					background-color: #1e2139;
+					box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+						0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+					li {
+						cursor: pointer;
+						font-size: 12px;
+						padding: 10px 20px;
+
+						&:hover {
+							color: #1e2139;
+							background-color: #fff;
+						}
+					}
+				}
+			}
+
+			.button {
+				padding: 8px 10px;
+				background-color: #7c5dfa;
+				border-radius: 40px;
+
+				.inner-button {
+					margin-right: 8px;
+					border-radius: 50%;
+					padding: 8px;
+					align-items: center;
+					justify-content: center;
+					background-color: #fff;
+
+					img {
+						width: 10px;
+						height: 10px;
+					}
+				}
+			}
+		}
+	}
+}
+</style>
