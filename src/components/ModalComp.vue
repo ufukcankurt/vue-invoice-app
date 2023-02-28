51f3a0a8419f14bae6295abfacb5db948d9612bd
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-02-28 20:38:09 +0300

    feat(components): create new modal-comp

---
+++ src/components/ModalComp.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="modal flex">
+		<div class="modal-content">
+			<p>Are you sure you want to exit? Your Changes will not be saved?</p>
+			<div class="actions flex">
+				<button
+					@click="closeModal"
+					class="purple"
+				>
+					Return
+				</button>
+				<button
+					@click="closeInvoice"
+					class="red"
+				>
+					Close
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+export default {
+	name: 'ModalComp',
+	methods: {
+		...mapMutations(['TOGGLE_MODAL', 'TOGGLE_INVOICE']),
+
+		closeModal() {
+			this.TOGGLE_MODAL();
+		},
+
+		closeInvoice() {
+			this.TOGGLE_MODAL();
+			this.TOGGLE_INVOICE();
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+	z-index: 100;
+	position: fixed;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	width: 100%;
+
+	.modal-content {
+		border-radius: 20px;
+		padding: 48px 32px;
+		max-width: 450px;
+		background-color: #252945;
+		color: #fff;
+
+		p {
+			text-align: center;
+		}
+
+		.actions {
+			margin-top: 24px;
+			button {
+				flex: 1;
+			}
+		}
+	}
+}
+</style>
