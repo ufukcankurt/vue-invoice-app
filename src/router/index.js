19deab1f0a40900222e17ff7343a9e87ae248e75
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-03-02 20:29:46 +0300

    feat(router): create new invoice route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import InvoiceView from "../views/InvoiceView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/invoice/:invoiceId",
+    name: "Invoice",
+    component: InvoiceView,
   },
 ];
 
