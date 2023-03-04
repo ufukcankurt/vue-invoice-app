e456a3b4541e8f28547e0e91d65f513ae9c527d3
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-03-04 12:06:15 +0300

    feat(store): create set-current-invoice state

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,7 @@ export default createStore({
     invoiceModal: null,
     modalActive: null,
     invoicesLoaded: null,
+    currentInvoiceArray: null,
   },
   getters: {},
   mutations: {
@@ -21,7 +22,12 @@ export default createStore({
     },
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
-    }
+    },
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
+        return invoice.invoiceId === payload;
+      });
+    },
   },
   actions: {
     async GET_INVOICES({ commit, state }) {
