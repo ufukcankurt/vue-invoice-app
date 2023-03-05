58f254ce14f949cf9c0360c1d4066c48e8ba1931
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-03-05 13:34:05 +0300

    feat: add methods to delete invoice

--- src/store/index.js
+++ src/store/index.js
@@ -79,6 +79,11 @@ export default createStore({
       commit("TOGGLE_EDIT_INVOICE");
       commit("SET_CURRENT_INVOICE", routeId);
     },
+    async DELETE_INVOICE({ commit }, docId) {
+      const getInvoice = db.collection("invoices").doc(docId);
+      await getInvoice.delete();
+      commit("DELETE_INVOICE", docId);
+    },
   },
   modules: {},
 });
--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -127,7 +127,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState } from 'vuex';
+import { mapActions, mapMutations, mapState } from 'vuex';
 export default {
 	name: 'InvoiceView',
 	data() {
@@ -146,6 +146,8 @@ export default {
 			'TOGGLE_INVOICE',
 		]),
 
+		...mapActions(['DELETE_INVOICE']),
+
 		getCurrentInvoice() {
 			this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
 			this.currentInvoice = this.currentInvoiceArray[0];
@@ -154,6 +156,11 @@ export default {
 		toggleEditInvoice() {
 			this.TOGGLE_EDIT_INVOICE();
 			this.TOGGLE_INVOICE();
+		},
+
+		async deleteInvoice(docId) {
+			await this.DELETE_INVOICE(docId);
+			this.$router.push({ name: 'home' });
 		},
 	},
 	computed: {
